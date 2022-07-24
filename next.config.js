4d373e31a86b600ec88065a24d45fdd934462c47
aito <BoxPistols@users.noreply.github.com>
2022-07-24 09:03:13 +0900

    Ass Sass with Submodule

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 // TODO: 必要？　↓
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+const nextConfig = {
+  reactStrictMode: true,
+}
 // module.exports = nextConfig
 
 const nextTranslate = require('next-translate')
-module.exports = nextTranslate()
+// module.exports = nextTranslate()
+const path = require('path')
+
+module.exports = [
+  nextTranslate(),
+  nextConfig,
+  {
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+  },
+]
