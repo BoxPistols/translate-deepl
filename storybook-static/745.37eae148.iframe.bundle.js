8dbe54cea93ef1b15d100db141c0abf6ccd4198a
aito <BoxPistols@users.noreply.github.com>
2023-03-04 23:12:34 +0900

    remove SB

--- storybook-static/745.37eae148.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknextjs_ts_mui_scss_storybook=self.webpackChunknextjs_ts_mui_scss_storybook||[]).push([[745],{"./node_modules/react-dom/client.js":(__unused_webpack_module,exports,__webpack_require__)=>{var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
