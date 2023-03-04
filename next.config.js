4029e8fbe4e8c6f096e4a2ea487cfb7e2d5acc72
aito <BoxPistols@users.noreply.github.com>
2023-03-04 23:59:23 +0900

    Clear Code

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-/** @type {import('next').NextConfig} */
-// TODO: 必要？　↓
 const nextConfig = {
   reactStrictMode: true,
 }
-// module.exports = nextConfig
-
-const nextTranslate = require('next-translate')
-// module.exports = nextTranslate()
 const path = require('path')
 
 module.exports = [
-  nextTranslate(),
   nextConfig,
   {
     sassOptions: {
