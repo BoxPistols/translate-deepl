ec3bad777587fad85f1ac6b7f0bb03f7003f1e75
aito <BoxPistols@users.noreply.github.com>
2023-03-12 18:01:05 +0900

    translate all case

--- src/components/Trans.tsx
+++ src/components/Trans.tsx
@@ -22,6 +22,14 @@ export const Translate = () => {
     setTranslatedText(data.translations[0].text)
   }
 
+  // const clear = () => {
+  //   return { translatedText: null }
+  // }
+
+  let capitalize = function (str) {
+    if (typeof str !== 'string' || !str) return str
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+  }
   return (
     <div>
       <input
@@ -29,10 +37,128 @@ export const Translate = () => {
         type="text"
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
+        placeholder="翻訳したい日本語を入れてください"
       />
-      <button onClick={handleTranslate}>Translate</button>
-      <p>
-        {translatedText}:{inputText}
+      <button
+        onClick={handleTranslate}
+        style={{ background: '#ccc', margin: '8px 2px', padding: '10px' }}
+      >
+        Translate
+      </button>
+
+      {/* <button
+        onClick={clear}
+        style={{ background: '#ccc', margin: '8px 2px', padding: '10px' }}
+      >
+        clear
+      </button> */}
+
+      <div className={styles.head4}>
+        Capitalize{' '}
+        <small style={{ fontSize: '12px' }}>
+          先頭（最初の1文字）を大文字、以降を小文字
+        </small>
+      </div>
+      {/* <p style={{ margin: '8px' }}> */}
+      <p
+        style={{
+          border: '2px solid #ccc',
+          padding: '8px',
+          userSelect: 'all',
+          marginBottom: 16,
+        }}
+      >
+        {'"'}
+        {/* {capitalize. */}
+        {capitalize(translatedText.replace(/\s+/g, ' '))}
+        {'"'}: {'"'}
+        {inputText}
+        {'"'}
+        <br />
+        {'"'}
+        {capitalize(translatedText.replace(/\s+/g, ' '))}
+        {'"'}: {'"'}
+        {capitalize(translatedText.replace(/\s+/g, ' '))}
+        {/* {translatedText} */}
+        {'"'}
+      </p>
+
+      <div className={styles.head4}>
+        All UpperCamel{' '}
+        <small style={{ fontSize: '12px' }}>単語ごとの先頭大文字</small>
+      </div>
+      <p
+        style={{
+          textTransform: 'capitalize',
+          border: '2px solid #ccc',
+          padding: '8px',
+          userSelect: 'all',
+          marginBottom: 16,
+        }}
+      >
+        {'"'}
+        {/* {capitalize. */}
+        {translatedText.replace(/\s+/g, ' ')}
+        {'"'}: {'"'}
+        {inputText}
+        {'"'}
+        <br />
+        {'"'}
+        {translatedText.replace(/\s+/g, ' ')}
+        {'"'}: {'"'}
+        {translatedText}
+        {'"'}
+      </p>
+
+      <div className={styles.head4}>
+        lower-kebabl <small style={{ fontSize: '12px' }}>小文字のケバブ</small>
+      </div>
+      <p
+        style={{
+          textTransform: 'lowercase',
+          border: '2px solid #ccc',
+          padding: '8px',
+          userSelect: 'all',
+          marginBottom: 16,
+        }}
+      >
+        {'"'}
+        {translatedText.replace(/\s+/g, '-')}
+        {'"'}: {'"'}
+        {inputText}
+        {'"'}
+        <br />
+        {'"'}
+        {translatedText.replace(/\s+/g, '-')}
+        {'"'}: {'"'}
+        {translatedText}
+        {'"'}
+      </p>
+
+      <div className={styles.head4}>
+        snake-case{' '}
+        <small style={{ fontSize: '12px' }}>小文字のスネークケース</small>
+      </div>
+      <p
+        style={{
+          textTransform: 'lowercase',
+          border: '2px solid #ccc',
+          padding: '8px',
+          userSelect: 'all',
+          marginBottom: 16,
+        }}
+      >
+        {'"'}
+        {translatedText.replace(/\s+/g, '_')}
+        {'"'}: {'"'}
+        {inputText}
+        {'"'}
+        <br />
+        {'"'}
+        {translatedText.replace(/\s+/g, '_')}
+        {'"'}: {'"'}
+        {translatedText}
+        {'"'}
       </p>
     </div>
   )
